58becb8ab610e212ad64d0bb241325481baf8fd3
Aditya.Pandit <adpndt@gmail.com>
2025-04-22 12:32:29 +0530

    initial project commit

---
+++ src/main/java/com/atozdev/uptimemoniter/UptimemoniterApplication.java
@@ -0,0 +1,13 @@
+package com.atozdev.uptimemoniter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimemoniterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimemoniterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atozdev/uptimemoniter/UptimemoniterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atozdev.uptimemoniter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimemoniterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
